9b47b1e32a6723d80850d116f07d41d62489b6a6
Pablo Andres Chacon <pabloandres120297@gmail.com>
2024-09-13 13:34:04 -0500

    excluyendo path de autenticacion

--- src/main/java/com/andres/curso/springboot/app/springboot_crud/entities/User.java
+++ src/main/java/com/andres/curso/springboot/app/springboot_crud/entities/User.java
@@ -2,6 +2,8 @@ package com.andres.curso.springboot.app.springboot_crud.entities;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -10,9 +12,12 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 import jakarta.persistence.UniqueConstraint;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name="users")
@@ -22,8 +27,12 @@ public class User {
   private Long id;
 
   @Column(unique = true)
+  @NotBlank
+  @Size(min = 4, max = 12)
   private String username;
 
+  @NotBlank
+  @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
   private String password;
 
   @ManyToMany
@@ -34,6 +43,15 @@ public class User {
     uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id", "role_id"})}
   )
   private List<Role> roles;
+
+  @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+  private boolean enabled;
+
+  @PrePersist
+  public void prePersist() {
+    enabled = true;
+  }
+
 
   @Transient
   private boolean admin;
@@ -76,6 +94,14 @@ public class User {
 
   public void setAdmin(boolean admin) {
     this.admin = admin;
+  }
+
+  public boolean isEnabled() {
+    return enabled;
+  }
+
+  public void setEnabled(boolean enabled) {
+    this.enabled = enabled;
   }
 
   
--- src/main/java/com/andres/curso/springboot/app/springboot_crud/security/SpringSecurityConfig.java
+++ src/main/java/com/andres/curso/springboot/app/springboot_crud/security/SpringSecurityConfig.java
@@ -2,8 +2,11 @@ package com.andres.curso.springboot.app.springboot_crud.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfig {
@@ -11,5 +14,18 @@ public class SpringSecurityConfig {
   @Bean
   PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    return http.authorizeHttpRequests((autorize) -> 
+      autorize
+      .requestMatchers("/api/users")
+      .permitAll()
+      .anyRequest()
+      .authenticated())
+      .csrf(config -> config.disable())
+      .sessionManagement(managment -> managment.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+      .build();
   }
 }
